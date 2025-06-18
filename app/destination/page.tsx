e47812a7d0bf1d36014f6ccc0d07da919bb34072
annguyen1909 <nguyentruongan1419@gmail.com>
2025-06-18 18:27:53 +0700

    deployment

--- app/destination/page.tsx
+++ app/destination/page.tsx
@@ -1,5 +1,13 @@
+'use client'
 import DestinationClient from '@/components/shared/DestinationClient';
+import { Suspense } from 'react';
 
 export default function DestinationPage() {
-  return <DestinationClient />;
+  return(
+    <div>
+      <Suspense>
+      <DestinationClient />;
+      </Suspense>
+    </div>
+  )
 }
